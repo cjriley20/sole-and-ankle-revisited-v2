495f2fd3cf7b26e15d237f3620e14ed995ee2a16
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-10-01 11:17:17 -0400

    Remove unnecessary height

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -61,7 +61,6 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   overflow-x: auto;
   overflow-y: hidden;
