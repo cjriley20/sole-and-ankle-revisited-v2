f54ebbadbcf4c98b09cb4b510261daba5b9d930e
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-09-30 22:08:48 -0400

    Finish mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -5,7 +5,7 @@ import * as Dialog from '@radix-ui/react-dialog';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
-import { COLORS } from '../../constants';
+import { COLORS, WEIGHTS } from '../../constants';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   return (
@@ -13,40 +13,29 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
       <Dialog.Portal>
         <Overlay />
         <Content>
-          <button onClick={onDismiss}>Dismiss menu</button>
-          <nav>
-            <a href="/sale">Sale</a>
-            <a href="/new">New&nbsp;Releases</a>
-            <a href="/men">Men</a>
-            <a href="/women">Women</a>
-            <a href="/kids">Kids</a>
-            <a href="/collections">Collections</a>
-          </nav>
-          <footer>
-            <a href="/terms">Terms and Conditions</a>
-            <a href="/privacy">Privacy Policy</a>
-            <a href="/contact">Contact Us</a>
-          </footer>
+          <CloseButton asChild>
+            <UnstyledButton>
+              <Icon id="close" />
+              <VisuallyHidden>Close</VisuallyHidden>
+            </UnstyledButton>
+          </CloseButton>
+          <Filler />
+          <Nav>
+            <NavLink href="/sale">Sale</NavLink>
+            <NavLink href="/new">New&nbsp;Releases</NavLink>
+            <NavLink href="/men">Men</NavLink>
+            <NavLink href="/women">Women</NavLink>
+            <NavLink href="/kids">Kids</NavLink>
+            <NavLink href="/collections">Collections</NavLink>
+          </Nav>
+          <Footer>
+            <FooterLink href="/terms">Terms and Conditions</FooterLink>
+            <FooterLink href="/privacy">Privacy Policy</FooterLink>
+            <FooterLink href="/contact">Contact Us</FooterLink>
+          </Footer>
         </Content>
       </Dialog.Portal>
     </Dialog.Root>
-
-    // <div>
-    //   <button onClick={onDismiss}>Dismiss menu</button>
-    //   <nav>
-    //     <a href="/sale">Sale</a>
-    //     <a href="/new">New&nbsp;Releases</a>
-    //     <a href="/men">Men</a>
-    //     <a href="/women">Women</a>
-    //     <a href="/kids">Kids</a>
-    //     <a href="/collections">Collections</a>
-    //   </nav>
-    //   <footer>
-    //     <a href="/terms">Terms and Conditions</a>
-    //     <a href="/privacy">Privacy Policy</a>
-    //     <a href="/contact">Contact Us</a>
-    //   </footer>
-    // </div>
   );
 };
 
@@ -64,6 +53,52 @@ const Content = styled(Dialog.Content)`
   bottom: 0;
   width: 300px;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 32px 32px;
+`;
+
+const CloseButton = styled(Dialog.Close)`
+  position: absolute;
+  top: 26px;
+  right: 16px;
+`;
+
+const Filler = styled.div`
+  flex: 1;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const NavLink = styled.a`
+  font-size: 1.125rem;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: flex-end;
+  gap: 14px;
+`;
+
+const FooterLink = styled.a`
+  font-size: 0.875rem;
+  color: ${COLORS.gray[700]};
+  text-decoration: none;
+  font-weight: ${WEIGHTS.normal};
 `;
 
 export default MobileMenu;
